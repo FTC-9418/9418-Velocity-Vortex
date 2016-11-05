3931109ee87d6ef51c6527aad585501445d8df1b
tempest-automatons <tempestautomatons@gmail.com>
2016-11-04 22:04:04 -0500

    Added Hardware and HolonomicDrive opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -0,0 +1,99 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This is NOT an opmode.
+ *
+ * This class can be used to define all the specific hardware for a single robot.
+ *
+ */
+public class Hardware
+{
+    public DcMotor fl = null;
+    public DcMotor bl = null;
+    public DcMotor fr = null;
+    public DcMotor br = null;
+    public DcMotor wl = null;
+    public DcMotor wr = null;
+    public Servo    push             = null;
+
+    public static final double MID_SERVO =  0.5 ;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  =  new ElapsedTime();
+
+    /* Constructor */
+    public Hardware(){
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Drivetrain Motors
+        fr = hwMap.dcMotor.get("FR");
+        fl = hwMap.dcMotor.get("FL");
+        br = hwMap.dcMotor.get("BR");
+        bl = hwMap.dcMotor.get("BL");
+
+        // Define and Initialize Winch Motors
+        wr = hwMap.dcMotor.get("WR");
+        wl = hwMap.dcMotor.get("WL");
+        wr.setDirection(DcMotor.Direction.REVERSE);
+
+        // Set all motors to zero power
+        fr.setPower(0);
+        fl.setPower(0);
+        br.setPower(0);
+        bl.setPower(0);
+        wr.setPower(0);
+        wl.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        wr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        wl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Define and initialize ALL installed servos.
+        push = hwMap.servo.get("push_servo");
+        push.setPosition(MID_SERVO);
+    }
+
+    /***
+     *
+     * waitForTick implements a periodic delay. However, this acts like a metronome with a regular
+     * periodic tick.  This is used to compensate for varying processing times for each cycle.
+     * The function looks at the elapsed cycle time, and sleeps for the remaining time interval.
+     *
+     * @param periodMs  Length of wait cycle in mSec.
+     */
+    public void waitForTick(long periodMs) {
+
+        long  remaining = periodMs - (long)period.milliseconds();
+
+        // sleep for the remaining portion of the regular cycle period.
+        if (remaining > 0) {
+            try {
+                Thread.sleep(remaining);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        // Reset the cycle clock for the next pass.
+        period.reset();
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HolonomicDrive.java
@@ -0,0 +1,161 @@
+/*
+Copyright (c) 2016 Robert Atkinson
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Robert Atkinson nor the names of his contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * This file provides basic Telop driving for a Holonomic drivetrain robot.
+ * The code is structured as an Iterative OpMode
+ *
+ * This OpMode uses the common hardware class to define the devices on the robot.
+ * All device access is managed through the Hardware class.
+ *
+ * This particular OpMode executes a holonomic drive Teleop for the robot
+ * It raises and lowers a arm using the Gampad Y and A buttons respectively.
+ * It also uses a servo connected to a rack and pinion system to push buttons on the beacon
+ *
+ */
+
+@TeleOp(name="Holonomic Drive", group="Drive")
+public class HolonomicDrive extends OpMode{
+
+    /* Declare OpMode members. */
+    Hardware robot = new Hardware(); // Use the class created to define the robot's hardware
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Test", "Foo Bar Fizz Buzz Xyzzy");
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        drive();
+        winch();
+        push();
+    }
+
+    public void drive(){
+        double x; // Right/ Left Movement
+        double y; // Forward/ Backward movement
+        double z; // Used for turning
+
+        x = -gamepad1.left_stick_x;
+        y = -gamepad1.left_stick_y;
+        z = -gamepad1.right_stick_x;
+        if(y >= 0.5) {
+            robot.fr.setPower(0.5);
+            robot.fl.setPower(0.5);
+            robot.br.setPower(0.5);
+            robot.bl.setPower(0.5);
+        } else if(y <= 0.5) {
+            robot.fr.setPower(0.5);
+            robot.fl.setPower(0.5);
+            robot.br.setPower(0.5);
+            robot.bl.setPower(0.5);
+        } else if(x >= 0.5) {
+            robot.fr.setPower(0.5);
+            robot.fl.setPower(0.5);
+            robot.br.setPower(0.5);
+            robot.bl.setPower(0.5);
+        } else if(x <= 0.5) {
+            robot.fr.setPower(0.5);
+            robot.fl.setPower(0.5);
+            robot.br.setPower(0.5);
+            robot.bl.setPower(0.5);
+        } else {
+            robot.fr.setPower(0);
+            robot.fl.setPower(0);
+            robot.br.setPower(0);
+            robot.bl.setPower(0);
+        }
+    }
+
+    public void winch(){
+        // Use gamepad buttons to move the arm up (Y) and down (A)
+        if (gamepad1.y) {
+            robot.wl.setPower(0.25);
+            robot.wr.setPower(0.25);
+        } else if (gamepad1.a) {
+            robot.wl.setPower(-0.25);
+            robot.wr.setPower(-0.25);
+        } else {
+            robot.wl.setPower(0);
+            robot.wr.setPower(0);
+        }
+    }
+
+    public void push() {
+        if(gamepad1.left_bumper) {
+            robot.push.setPosition(1);
+        } else if(gamepad1.right_bumper) {
+            robot.push.setPosition(-1);
+        } else {
+            robot.push.setPosition(0.5);
+        }
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+}
